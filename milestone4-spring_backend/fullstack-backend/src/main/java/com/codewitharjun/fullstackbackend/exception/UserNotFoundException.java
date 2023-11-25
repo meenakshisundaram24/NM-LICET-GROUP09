eec97e80c2a16823d01711ba73711645fef782a8
C.Meenakshi Sundaram <96050296+meenakshisundaram24@users.noreply.github.com>
2023-11-25 20:28:17 +0530

    Delete milestone4-spring_backend/fullstack-backend directory

--- milestone4-spring_backend/fullstack-backend/src/main/java/com/codewitharjun/fullstackbackend/exception/UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.codewitharjun.fullstackbackend.exception;
-
-@SuppressWarnings("serial")
-public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(String id){
-        super("Could not found the user with id "+ id);
-    }
-}
