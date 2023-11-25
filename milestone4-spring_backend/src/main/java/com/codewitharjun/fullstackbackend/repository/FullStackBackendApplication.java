266c35f6846691182f17662c3c85690f2b75036f
C.Meenakshi Sundaram <96050296+meenakshisundaram24@users.noreply.github.com>
2023-11-25 20:32:49 +0530

    Add files via upload

---
+++ milestone4-spring_backend/src/main/java/com/codewitharjun/fullstackbackend/repository/FullStackBackendApplication.java
@@ -0,0 +1,14 @@
+package com.codewitharjun.fullstackbackend.repository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class FullStackBackendApplication {
+	public static void main(String[] args)
+	{
+		SpringApplication.run(FullStackBackendApplication.class);
+	}
+
+}
